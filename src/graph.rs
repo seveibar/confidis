12babb314d0fa51554bd2554bb7a70515059a569
seveibar <seveibar@gmail.com>
2020-07-11 20:00:51 -0400

    refactoring graph to have equalifier

--- src/graph.rs
+++ src/graph.rs
@@ -1,5 +1,6 @@
-use crate::command::{Command, CommandType, Answer};
 use crate::cluster::compute_clusters;
+use crate::command::{Answer, Command, CommandType};
+use crate::equalifier::{Equalifier, ExactEqualifier};
 
 use std::collections::HashMap;
 use std::result::Result;
@@ -20,11 +21,11 @@ pub struct Question {
     answer: Option<Answer>,
     confidence: f64,
     sources: Vec<SourceId>,
-    answers: HashMap<SourceId, Answer>
+    answers: HashMap<SourceId, Answer>,
 }
 
 #[derive(Debug)]
-pub struct Graph {
+pub struct Graph<'a> {
     sources: HashMap<String, Source>,
     questions: HashMap<String, Question>,
     influence_source_confidence: f64,
@@ -32,7 +33,8 @@ pub struct Graph {
     initial_source_strength: i64,
     strength_maximum: i64,
     influence_question_strength: i64,
-    answer_count: u64
+    answer_count: u64,
+    equalifier: &'a Equalifier,
 }
 
 impl Graph {
@@ -45,41 +47,70 @@ impl Graph {
             initial_source_strength: 1,
             strength_maximum: 100,
             influence_question_strength: 1,
-            answer_count: 0
+            answer_count: 0,
+            equalifier: ExactEqualifier::new(),
         }
     }
     pub fn execute_command(&mut self, cmd: &Command) -> Result<String, &str> {
         match cmd.cmd {
             CommandType::Set => {
                 if !self.sources.contains_key(&cmd.source) {
-                    self.sources.insert(cmd.source.to_string(), Source {
-                        name: cmd.source.to_string(),
-                        quality: self.default_source_quality,
-                        strength: self.initial_source_strength
-                    });
+                    self.sources.insert(
+                        cmd.source.to_string(),
+                        Source {
+                            name: cmd.source.to_string(),
+                            quality: self.default_source_quality,
+                            strength: self.initial_source_strength,
+                        },
+                    );
                 }
                 if !self.questions.contains_key(&cmd.question) {
-                    self.questions.insert(cmd.question.to_string(), Question {
-                        name: cmd.question.to_string(),
-                        answer: None,
-                        confidence: 0.0,
-                        sources: vec![],
-                        answers: HashMap::new()
-                    });
+                    self.questions.insert(
+                        cmd.question.to_string(),
+                        Question {
+                            name: cmd.question.to_string(),
+                            answer: None,
+                            confidence: 0.0,
+                            sources: vec![],
+                            answers: HashMap::new(),
+                        },
+                    );
                 }
                 let mut source = self.sources.get_mut(&cmd.source).unwrap();
                 let mut question = self.questions.get_mut(&cmd.question).unwrap();
 
                 // Update source quality if answer has high enough confidence
-                if question.answer.is_some() && question.confidence > self.influence_source_confidence {
+                if question.answer.is_some()
+                    && question.confidence > self.influence_source_confidence
+                {
                     // update source quality
                     // TODO put distance function here & clustering
-                    if &cmd.answer == &question.answer.as_ref().unwrap().content {
-                        println!("CORRECT: source ({}) matches answer for question ({})", cmd.source, cmd.question);
-                        source.quality = (source.quality * source.strength as f64 + 1.0) / (source.strength as f64 + 1.0);
+                    let answer_distance =
+                        self.equalifier.get_distance(&cmd.answer, &question.answer);
+                    if answer_distance < 1 {
+                        println!(
+                            "CORRECT: source ({}) matches answer for question ({})",
+                            cmd.source, cmd.question
+                        );
+                        let new_source_quality = (source.quality * source.strength as f64 + 1.0)
+                            / (source.strength as f64 + 1.0);
+                        println!(
+                            "Adjusting {}.quality {} -> {}",
+                            source.name, source.quality, new_source_quality
+                        );
+                        source.quality = new_source_quality;
                     } else {
-                        println!("WRONG: source ({}) doesn't match answer for question ({})", cmd.source, cmd.question);
-                        source.quality = (source.quality * source.strength as f64) / (source.strength as f64 + 1.0);
+                        println!(
+                            "WRONG: source ({}) doesn't match answer for question ({})",
+                            cmd.source, cmd.question
+                        );
+                        let new_source_quality = (source.quality * source.strength as f64)
+                            / (source.strength as f64 + 1.0);
+                        println!(
+                            "Adjusting {}.quality {} -> {}",
+                            source.name, source.quality, new_source_quality
+                        );
+                        source.quality = new_source_quality;
                     }
                     if source.strength < self.strength_maximum {
                         source.strength += 1;
@@ -91,13 +122,12 @@ impl Graph {
 
                 question.answers.insert(
                     cmd.source.to_string(),
-                    Answer::new(cmd.answer.clone(), cmd.source.clone())
+                    Answer::new(cmd.answer.clone(), cmd.source.clone()),
                 );
 
                 if source.strength > self.influence_question_strength {
                     // Compute the probability of each answer
                     // HashMap<AnswerId, Vec<Double>>
-
 
                     // TODO update connected sources?
                     // for each connected source {
@@ -108,13 +138,17 @@ impl Graph {
                 // Update question if it has enough sources
                 // TODO
 
-
-                return Ok(String::from(""))
+                return Ok(String::from(""));
             }
             CommandType::Get => {
-                let question:&Question = self.questions.get(&cmd.question).unwrap();
+                let question: &Question = self.questions.get(&cmd.question).unwrap();
                 let default_answer: Answer = Answer::new(String::from("None"), String::from(""));
-                Ok(format!("{} {} {}", question.name, question.confidence, question.answer.as_ref().unwrap_or(&default_answer)))
+                Ok(format!(
+                    "{} {} {}",
+                    question.name,
+                    question.confidence,
+                    question.answer.as_ref().unwrap_or(&default_answer)
+                ))
                 // return Ok(String::from("Hi"))
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-mod command;
 mod cluster;
+mod command;
 mod equalifier;
 mod graph;
 
 // use std::io;
+use command::{Command, CommandType};
 use std::env;
 use std::fs;
-use command::{ Command, CommandType };
 
 fn convert_to_command(line: &str) -> Command {
     let items: Vec<&str> = line.split_whitespace().collect();
@@ -31,33 +31,30 @@ fn convert_to_command(line: &str) -> Command {
                 distribution: String::from("default"),
             };
         }
-        _ => panic!("Invalid command: {}", items[0])
+        _ => panic!("Invalid command: {}", items[0]),
     }
 }
 
 fn main() {
-
     let args: Vec<String> = env::args().collect();
 
     let filename = &args[1];
 
-    let contents = fs::read_to_string(filename)
-        .expect("Couldn't read file");
+    let contents = fs::read_to_string(filename).expect("Couldn't read file");
 
     let lines = contents.lines().filter(|line| !line.is_empty());
 
-    let commands:Vec<Command> = lines.map(|line| convert_to_command(line)).collect();
+    let commands: Vec<Command> = lines.map(|line| convert_to_command(line)).collect();
 
     let mut g = graph::Graph::new();
 
     for command in &commands {
-        let output = g.execute_command(&command)
+        let output = g
+            .execute_command(&command)
             .expect("Couldn't execute command");
 
         println!("{}", output);
-
     }
 
     // println!("{:?}", commands);
-
 }
