5188cc0c1d9f1c6518f634733d9e683f41fc2c71
seveibar <seveibar@gmail.com>
2020-07-20 14:41:16 -0400

    remove old build-npm script

--- build-npm.sh
+++
@@ -1,7 +0,0 @@
-rm -rf ./pkg
-mkdir ./pkg
-wasm-pack build -t nodejs -d ./pkg/node
-wasm-pack build -t bundler -d ./pkg/webpack
-# TODO need to fix ./pkg/webpack/package.json "files" to include "confidis_bg.js"
-cp ./package.json ./pkg/package.json
-touch ./pkg/.npmignore
