528a58cffc9dc224a2160573347b06ab0b2870d7
seveibar <seveibar@gmail.com>
2020-11-14 23:11:42 -0500

    fix: attempt release of subfolder

--- releaserc.js
+++ releaserc.js
@@ -3,7 +3,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    ["@semantic-release/npm", { npmPublish: true, pkgRoot: "pkg" }],
+    ["@semantic-release/npm", { npmPublish: true, pkgRoot: "./pkg" }],
     "@semantic-release/github",
     [
       "@semantic-release/git",
